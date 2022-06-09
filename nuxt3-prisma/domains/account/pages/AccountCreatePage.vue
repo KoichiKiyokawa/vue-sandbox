a0f5dd8b1e83d6a217acc91c9b30fe2579a8f3dc
Koichi Kiyokawa <koichi20110068@gmail.com>
2022-06-09 23:39:26 +0900

    chore: install nuxt-windicss

--- nuxt3-prisma/domains/account/pages/AccountCreatePage.vue
+++ nuxt3-prisma/domains/account/pages/AccountCreatePage.vue
@@ -11,7 +11,7 @@ const onSubmit = () => {
   submitting.value = true
   $fetch("/api/create-account", { method: "post", body: form })
     .then()
-    .catch()
+    .catch((e) => alert(e))
     .finally(() => {
       submitting.value = false
     })
@@ -19,8 +19,29 @@ const onSubmit = () => {
 </script>
 
 <template lang="pug">
-form(@submit.prevent="onSubmit")
-  input(v-model="form.username")
-  input(type="password" v-model="form.password")
-  button
+form(@submit.prevent="onSubmit" class="border rounded-md shadow-lg p-4 container mx-auto")
+  label
+    span user name
+    input(v-model="form.username")
+  label
+    span password
+    input(type="password" v-model="form.password")
+  button(:disabled="submitting" class="text-white bg-blue-400 p-2 mt-6 rounded") submit
 </template>
+
+<style scoped>
+label {
+  @apply block;
+}
+label + label {
+  @apply mt-4;
+}
+
+label span {
+  @apply block mb-2;
+}
+
+input {
+  @apply border p-2 w-full;
+}
+</style>
